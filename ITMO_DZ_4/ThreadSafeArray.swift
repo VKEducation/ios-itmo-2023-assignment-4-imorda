80e59db8d174e91ac5baa93432a21854076dbb2b
imodre <btima@mail.ru>
2023-12-03 23:16:37 +0300

    Task done

--- ITMO_DZ_4/ThreadSafeArray.swift
+++ ITMO_DZ_4/ThreadSafeArray.swift
@@ -1,21 +1,58 @@
 import Foundation
 
-class ThreadSafeArray<T>  {
+class ThreadSafeArray<T> {
     private var array: [T] = []
+    private let lock = RWLock()
+    
+    public func append(_ value: Element) {
+        lock.withWriteLock { array.append(value) }
+    }
+    
+    @discardableResult
+    public func remove(at index: Int) -> Element {
+        lock.withWriteLock { array.remove(at: index) }
+    }
+    
+    public func insert(_ newElement: Element, at i: Int) {
+        lock.withWriteLock { array.insert(newElement, at: i) }
+    }
+    
+    public func removeAll(keepingCapacity keepCapacity: Bool = false) {
+        lock.withWriteLock { array.removeAll() }
+    }
+    
+    public func reserveCapacity(_ minimumCapacity: Int) {
+        lock.withWriteLock { array.reserveCapacity(minimumCapacity) }
+    }
+    
+    public func toString() -> String {
+        lock.withReadLock { "\(array)" }
+    }
 }
 
-extension ThreadSafeArray: RandomAccessCollection {
+extension ThreadSafeArray: RandomAccessCollection, MutableCollection {
     typealias Index = Int
     typealias Element = T
-
-    var startIndex: Index { return array.startIndex }
-    var endIndex: Index { return array.endIndex }
-
-    subscript(index: Index) -> Element {
-        get { return array[index] }
+    
+    public var startIndex: Int {
+        lock.withReadLock { array.startIndex }
+    }
+    
+    public var endIndex: Int {
+        lock.withReadLock { array.endIndex }
     }
 
-    func index(after i: Index) -> Index {
-        return array.index(after: i)
+    public subscript(position: Int) -> T {
+        get {
+            lock.withReadLock { array[position] }
+        }
+        
+        set {
+            lock.withWriteLock { array[position] = newValue }
+        }
+    }
+        
+    public func index(after i: Index) -> Index {
+        lock.withReadLock { array.index(after: i) }
     }
 }
--- ITMO_DZ_4/main.swift
+++ ITMO_DZ_4/main.swift
@@ -1,9 +1,10 @@
 import Foundation
 
+/* What is that for??
 let ff = ThreadSafeArray<Int>()
 var dd = [1]
 dd.append(1)
 ff.dropFirst()
 
 let sd = ff[0]
-
+*/
